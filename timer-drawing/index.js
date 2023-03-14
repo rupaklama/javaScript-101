317005dcfe847948872d87a6cf0fb1ebb199ca16
pak <rupaklamadev@gmail.com>
2023-03-14 18:20:35 -0400

    timer app completed

--- timer-drawing/index.js
+++ timer-drawing/index.js
@@ -1,9 +1,27 @@
+// Elements
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+const circleSVG = document.querySelector('circle');
+
+// circle radius
+const perimeter = circleSVG.getAttribute('r') * 2 * Math.PI;
+// drawing circle
+circleSVG.setAttribute('stroke-dasharray', perimeter);
+
 // NOTE - using Class effectively around DOM Elements to generate Events
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    // making callbacks optional in this class
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     // note - Passed in Argument in bind, call & apply will override the default value of 'this' inside of a function
     // note - this is the older way, using Arrow function is the modern way now
@@ -27,13 +45,17 @@ class Timer {
 
   // modern way - note: This will get added to the class constructor above
   start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+
     // first start it immediately
     this.tick();
 
     // continue on every seconds
     // note - rather than assigning value to a variable like this - const timer
     // assigning value by creating a 'intervalId' property in our object instance
-    this.intervalId = setInterval(this.tick, 1000);
+    this.intervalId = setInterval(this.tick, 20);
   };
 
   pause = () => {
@@ -44,11 +66,19 @@ class Timer {
     // getter
     const timeRemaining = this.timeRemaining;
 
-    // setter
     if (this.timeRemaining <= 0) {
       this.pause();
+
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
-      this.timeRemaining = timeRemaining - 1;
+      // setter
+      this.timeRemaining = timeRemaining - 0.02;
+
+      if (this.tick) {
+        this.onTick(this.timeRemaining);
+      }
     }
     // same as above
     // this.timeRemaining = this.timeRemaining - 1
@@ -62,15 +92,32 @@ class Timer {
 
   // Setter method - to set a value
   set timeRemaining(time) {
-    this.durationInput.value = time;
+    this.durationInput.value = time.toFixed(2);
   }
 }
 
-// Elements
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
+// default value
+let duration;
 
 // new object instance
-const timer = new Timer(durationInput, startButton, pauseButton);
-// timer.start();
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  // Optional Object - to keep track of events to update the UI
+  // passing callbacks here as events that will be invoked on timer life cycle phases
+  onStart(totalDuration) {
+    // draw the border
+    duration = totalDuration;
+  },
+
+  onTick(timeRemaining) {
+    // update the border with spaces
+    circleSVG.setAttribute(
+      'stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+
+  onComplete() {
+    // reset the border
+    console.log('timer just completed');
+  },
+});
