30191efac6cb2bb9c6c50accaedd5f92465cf384
Rupak Lama <rupaklamadev@gmail.com>
2023-06-14 17:28:02 -0400

    Add Cart page

--- ecomm/routes/carts/carts.js
+++ ecomm/routes/carts/carts.js
@@ -1,10 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/cart/products', (req, res) => {
-  // console.log(req.body.productId);
+const cartsRepo = require('../../repositories/carts');
+const productsRepo = require('../../repositories/products');
+const chartShowTemplate = require('../../views/carts/showCart');
 
-  res.status(200).send('Product added to cart');
+router.post('/cart/products', async (req, res) => {
+  // console.log(req.body.productId);
+  let cart;
+  if (!req.session.cartId) {
+    // On new session, create the cart & store the cart id into cookie
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  const existingItem = cart.items.find((item) => item.id === req.body.productId);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, { items: cart.items });
+  res.status(200).redirect('/cart');
+});
+
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
+  }
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+    item.product = product;
+  }
+  res.status(200).send(chartShowTemplate({ items: cart.items }));
+});
+
+router.post('/cart/products/delete', async (req, res) => {
+  const { itemId } = req.body;
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.status(200).redirect('/cart');
 });
 
 module.exports = router;
--- message-app/index.js
+++ message-app/index.js
@@ -1,23 +1,25 @@
 // elements
 const form = document.querySelector('form');
-const input = document.querySelector('input');
+const input = document.querySelector('#input');
 const linkInput = document.querySelector('#link-input');
 
 const messageForm = document.querySelector('#message-form');
 const messageShow = document.querySelector('#message-show');
 const linkForm = document.querySelector('#link-form');
 
-// # - to store data temporarily in the browser
-const { hash } = window.location;
+// http://127.0.0.1:5501/message-app/index.html#YWRmZA==
+// # - Hash/Fragment is to store data temporarily in the browser
+const { hash } = window.location; // "#YWRmZA=="
 // console.log(atob(hash.replace('#', '')));
 
+// Decoding - atob() function is to convert Base64 into ASCII - normal characters
 const message = atob(hash.replace('#', ''));
 
 if (message) {
   messageForm.classList.add('hide');
   messageShow.classList.remove('hide');
 
-  document.querySelector('h1').innerHTML = message;
+  document.querySelector('h1').innerText = message;
 }
 
 form.addEventListener('submit', e => {
@@ -28,13 +30,12 @@ form.addEventListener('submit', e => {
 
   // note - ASCII characters into base64
   // Encoding - btoa() method creates a Base64-encoded string
+  // btoa() - default browser api method to convert Normal Characters (ascii) into Base64 characters
   const encrypted = btoa(input.value);
   linkInput.value = `${window.location}#${encrypted}`;
 
   // to auto select input value
   linkInput.select();
-
-  // Decoding - atob() function decodes a string of data which has been encoded using Base64 encoding
 });
 
 // note - when console logging window.location object as a string, we get current URL href PATH
